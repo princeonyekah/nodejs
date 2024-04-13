86536e09e938118ce3b60d960ce926fc3d4168cf
Princewill <princewill.onyekah@kibo.school>
2024-04-13 19:26:37 +0100

    first commit

---
+++ node.js
@@ -0,0 +1,21 @@
+// const amount = 9
+
+// if (amount < 10){
+//     console.log("small number");
+// }
+// else{
+//     console.log("large number");
+// }
+
+// console.log(`hey it's my first node app`);
+
+// console.log(__dirname)
+// setInterval(() =>{
+//     console.log('hello world')
+// }, 1000)
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+module.exports = sayHi
---
+++ sec-module.js
@@ -0,0 +1,7 @@
+const names = require('./first-modules');
+const sayHi = require('./node')
+
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
